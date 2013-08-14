be8e8feb5ccf239df88c46d63e2ca5d9bc299c53
nattaporn <gogorenger@gmail.com>
2013-08-15 01:40:41 +0700

    change FunSuite to FreeSpec

--- src/test/scala/SeqSpec.scala
+++ src/test/scala/SeqSpec.scala
@@ -1,23 +1,26 @@
 import org.scalatest._
 import org.scalatest.matchers._
 
-class SeqSpec extends FunSuite with GivenWhenThen with  ShouldMatchers{
-	test("new") {
-		given("new seq(1,2,3)")
-		val seq = Seq(1,2,3)
-		when("check size")
-		val size = seq.size
-		then("size should be 3")
-		size should be (3)
+class SeqSpec extends FreeSpec with GivenWhenThen with  ShouldMatchers{
+	"A Seq" - {
+		"New Seq" in {
+			given("new seq(1,2,3)")
+			val seq = Seq(1,2,3)
+			when("check size")
+			val size = seq.size
+			then("size should be 3")
+			size should be (3)
+		}
+		"Add new value" in {
+			given("seq(1,2,3)")
+			val a = Seq(1,2,3)
+			when("add new value")
+			val b = a :+ 4
+			val size = b.size
+			println("## ->" + b)
+			then("size should be 4")
+			size should be (4)
+		}
 	}
-	test("add"){
-		given("seq(1,2,3)")
-		val a = Seq(1,2,3)
-		when("add new value")
-		val b = a :+ 4
-		val size = b.size
-		println("## ->" + b)
-		then("size should be 4")
-		size should be (4)
-	}
+	
 }
