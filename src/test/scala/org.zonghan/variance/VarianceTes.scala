eed821a167600d353470fb5dc99b0d95d7191c7a
Zonghan(Wu Zonghan) <zwu@coupang.com>
2017-07-05 16:10:46 +0800

    learn

--- src/test/scala/org.zonghan/variance/VarianceTes.scala
+++ src/test/scala/org.zonghan/variance/VarianceTes.scala
@@ -3,6 +3,7 @@ package org.zonghan.variance
 /**
   * Created by wuzonghan on 17/2/15.
   * http://www.cnblogs.com/amunote/p/5693782.html
+  * http://colobu.com/2015/05/19/Variance-lower-bounds-upper-bounds-in-Scala/
   */
 object VarianceTes extends App {
   // 将子类对象赋值给超类称为协变；将超类对象赋值给子类称为逆变。默认情况下，scala对这两种“变”都不支持。
