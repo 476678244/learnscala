35233ca726d82db67db1f8741181f1f3613f4958
Zonghan(Wu Zonghan) <zwu@coupang.com>
2018-08-29 15:37:22 +0800

    parallel list

--- src/test/scala/org.zonghan/nestfunction/FilterTest.scala
+++ src/test/scala/org.zonghan/nestfunction/FilterTest.scala
@@ -4,6 +4,7 @@ package org.zonghan.nestfunction
   * Created by wuzonghan on 17/2/14.
   */
 object FilterTest extends App {
+  Thread.sleep(10000)
   def filter(xs: List[Int], threshold: Int) = {
     def process(ys: List[Int]): List[Int] =
       if (ys.isEmpty) ys
@@ -15,11 +16,11 @@ object FilterTest extends App {
 
 }
 
-object FilterTest2 extends App {
-  def filter(xs: List[Int], threshold: Int): List[Int] = {
-    if (xs.isEmpty) xs
-    else if (xs.head < threshold) xs.head :: filter(xs.tail, threshold)
-    else filter(xs.tail, threshold)
-  }
-  println(filter(List(1, 9, 2, 8, 3, 7, 4), 5))
-}
+// object FilterTest2 extends App {
+//   def filter(xs: List[Int], threshold: Int): List[Int] = {
+//     if (xs.isEmpty) xs
+//     else if (xs.head < threshold) xs.head :: filter(xs.tail, threshold)
+//     else filter(xs.tail, threshold)
+//   }
+//   println(filter(List(1, 9, 2, 8, 3, 7, 4), 5))
+// }
