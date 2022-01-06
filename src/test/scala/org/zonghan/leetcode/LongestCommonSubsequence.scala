78e1aa7afde99446c11826cc634c0402dd6f499a
Zonghan Wu <a476678244@163.com>
2022-01-06 14:10:35 +0800

    return str for LongestCommonSubsequence

--- src/test/scala/org/zonghan/leetcode/LongestCommonSubsequence.scala
+++ src/test/scala/org/zonghan/leetcode/LongestCommonSubsequence.scala
@@ -26,9 +26,10 @@ package org.zonghan.leetcode
 object LongestCommonSubsequence extends App {
 
   //https://leetcode-cn.com/problems/longest-common-subsequence/
-  def longestCommonSubsequence(text1: String, text2: String): Int = {
+  def longestCommonSubsequence(text1: String, text2: String): (Int, String) = {
     val m = text1.length()
     val n = text2.length()
+    var commonSub = ""
     val dp = Array.ofDim[Int](m + 1, n + 1)
     for (i <- 1 to m) {
       val c1 = text1.charAt(i - 1)
@@ -36,18 +37,43 @@ object LongestCommonSubsequence extends App {
         val c2 = text2.charAt(j - 1)
         if (c1 == c2) {
           dp(i)(j) = dp(i - 1)(j - 1) + 1
+          if (dp(i)(j) > Math.max(dp(i - 1)(j), dp(i)(j - 1))) {
+            commonSub = commonSub + c2
+          }
         } else {
           dp(i)(j) = Math.max(dp(i - 1)(j), dp(i)(j - 1))
         }
       }
     }
-    dp(m)(n)
+    (dp(m)(n), commonSub)
   }
 
 //  println(longestCommonSubsequence("abcdbe", "bafcqelb"))
   // abcbe , baceb
   //
-  //  println(longestCommonSubsequence("abcde", "ace"))
-    println(longestCommonSubsequence("abceeffcbcba", "abc"))
+    println(longestCommonSubsequence("abcde", "ace"))
+//    println(longestCommonSubsequence("aac", "aaa"))
   //  println(longestCommonSubsequence("abc", "def"))
+
+  def longestCommonContinuousSubsequence(text1: String, text2: String): (Int, String) = {
+    val m = text1.length()
+    val n = text2.length()
+    var commonSub = ""
+    val dp = Array.ofDim[Int](m + 1, n + 1)
+    for (i <- 1 to m) {
+      val c1 = text1.charAt(i - 1)
+      for (j <- 1 to n) {
+        val c2 = text2.charAt(j - 1)
+        if (c1 == c2) {
+          dp(i)(j) = dp(i - 1)(j - 1) + 1
+          if (dp(i)(j) > Math.max(dp(i - 1)(j), dp(i)(j - 1))) {
+            commonSub = commonSub + c2
+          }
+        } else {
+          dp(i)(j) = Math.max(dp(i - 1)(j), dp(i)(j - 1))
+        }
+      }
+    }
+    (dp(m)(n), commonSub)
+  }
 }
