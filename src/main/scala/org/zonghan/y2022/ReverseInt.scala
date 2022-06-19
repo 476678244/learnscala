2ce2e41f6ad09826eb1537278b1ef2fa20605614
Zonghan Wu <a476678244@163.com>
2022-06-19 19:06:09 +0800

    ReverseInt solution

--- src/main/scala/org/zonghan/y2022/ReverseInt.scala
+++ src/main/scala/org/zonghan/y2022/ReverseInt.scala
@@ -1,5 +1,6 @@
 package org.zonghan.y2022
 
+// https://leetcode.cn/problems/reverse-integer/
 object ReverseInt extends App {
   /*
   * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
@@ -22,11 +23,16 @@ object ReverseInt extends App {
   * */
 
   def reverse(x: Int): Int = {
-    0
+    if (x == 0) return 0
+    val absX = if (x > 0) x.toLong else 0 - x.toLong
+    var raw = absX.toString.toCharArray.reverse
+    while (raw.head == '0') raw = raw.drop(1)
+    if (raw.mkString("").toLong > Math.pow(2, 31)) return 0
+    if (x > 0) raw.mkString("").toInt else ("-" + raw.mkString("")).toInt
   }
 
-  println(reverse(123))
-  println(reverse(-123))
-  println(reverse(120))
-  println(reverse(0))
+//  println(reverse(12300))
+//  println(reverse(-123))
+//  println(reverse(120))
+  println(reverse(-2147483648))
 }
