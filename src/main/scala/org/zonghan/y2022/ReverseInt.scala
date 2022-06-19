e27ab37ca11f7cf1278b359c1070be1f5424b510
Zonghan Wu <a476678244@163.com>
2022-06-19 17:38:10 +0800

    question ReverseInt

---
+++ src/main/scala/org/zonghan/y2022/ReverseInt.scala
@@ -0,0 +1,32 @@
+package org.zonghan.y2022
+
+object ReverseInt extends App {
+  /*
+  * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+如果反转后整数超过 32 位的有符号整数的范围[−231, 231− 1] ，就返回 0。
+假设环境不允许存储 64 位整数（有符号或无符号）。
+
+示例 1：
+输入：x = 123
+输出：321
+示例 2：=
+输入：x = -123
+输出：-321
+示例 3：
+输入：x = 120
+输出：21
+示例 4：
+输入：x = 0
+输出：0
+
+  * */
+
+  def reverse(x: Int): Int = {
+    0
+  }
+
+  println(reverse(123))
+  println(reverse(-123))
+  println(reverse(120))
+  println(reverse(0))
+}
