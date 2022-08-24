dacc05cbccb2c4d5b1643c47dde1c63a41ce0d8d
Zonghan Wu <a476678244@163.com>
2022-08-24 19:38:41 +0800

    question of LetterCombinations

---
+++ src/main/scala/org/zonghan/y2022/LetterCombinations.scala
@@ -0,0 +1,28 @@
+package org.zonghan.y2022
+
+//https://leetcode.cn/problems/letter-combinations-of-a-phone-number/
+object LetterCombinations extends App {
+
+  /*
+  * 给定一个仅包含数字2-9的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+    给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+    示例 1：
+    输入：digits = "23"
+    输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+    示例 2：
+    输入：digits = ""
+    输出：[]
+    示例 3：
+    输入：digits = "2"
+    输出：["a","b","c"]
+  * */
+  def letterCombinations(digits: String): List[String] = {
+    List("")
+  }
+
+  println(letterCombinations("23").mkString(","))
+
+  println(letterCombinations("").mkString(","))
+
+  println(letterCombinations("2").mkString(","))
+}
